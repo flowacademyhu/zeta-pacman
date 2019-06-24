f9bf1d58070e418f7fbc8ae7ecab9447a9dd80e0
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-06-24 20:37:34 +0200

    pacman move

--- move.js
+++ move.js
@@ -12,48 +12,70 @@ let pacman = {
   y: 2
 };
 
+let ghost = {
+  x: 3,
+  y: 3
+};
+
+let ghostDirect = ['up', 'down', 'left', 'right'];
+
 let direct = 'w'; // beolvasott irány
 let score = 0;
 
-switch (direct) {
-  case 'w':
+const move = (direct, arr, score, pacman) => {
+  switch (direct) {
+    case 'w':
+      if (arr[pacman.x - 1][pacman.y] === 3) { // ha ott ahová megy van kaja, akkor kapunk 10 pontot
+        score = score + 10;
+      }
+      if (arr[pacman.x - 1][pacman.y] === 5) {
 
-    if (arr[pacman.x - 1][pacman.y] === 3) { // ha ott ahová megy van kaja, akkor kapunk 10 pontot
-      score = score + 10;
-    }
-    if (arr[pacman.x - 1][pacman.y] === 5) {
+      } else {
+        arr[pacman.x][pacman.y] = 0;// ahol pacman eddig állt, az üres lesz
+        arr[pacman.x - 1][pacman.y] = 1; // ahová pacman megy, ott pacman lesz
+        pacman.x = pacman.x - 1;// pacman koordinátája
+      }
+      break;
+    case 's':
+      if (arr[pacman.x + 1][pacman.y] === 3) { // ha ott ahová megy van kaja, akkor kapunk 10 pontot
+        score = score + 10;
+      }
+      if (arr[pacman.x + 1][pacman.y] === 5) {
 
-    } else {
-      arr[pacman.x][pacman.y] = 0;// ahol pacman eddig állt, az üres lesz
-      arr[pacman.x - 1][pacman.y] = 1; // ahová pacman megy, ott pacman lesz
-      pacman.x = pacman.x - 1;// pacman koordinátája
-    }
-    break;
-  case 's':
-    arr[pacman.x][pacman.y] = 0; // ahol pacman eddig állt, az üres lesz
-    if (arr[pacman.x + 1][pacman.y] === 3) { // ha ott ahová megy van kaja, akkor kapunk 10 pontot
-      score = score + 10;
-    }
-    arr[pacman.x + 1][pacman.y] = 1; // ahová pacman megy, ott pacman lesz
-    pacman.x = pacman.x + 1; // pacman koordinátája
-    break;
-  case 'a':
-    arr[pacman.x][pacman.y] = 0; // ahol pacman eddig állt, az üres lesz
-    if (arr[pacman.x][pacman.y - 1] === 3) { // ha ott ahová megy van kaja, akkor kapunk 10 pontot
-      score = score + 10;
-    }
-    arr[pacman.x][pacman.y - 1] = 1; // ahová pacman megy, ott pacman lesz
-    pacman.y = pacman.y - 1; // pacman koordinátája
-    break;
-  case 'd':
-    arr[pacman.x][pacman.y] = 0; // ahol pacman eddig állt, az üres lesz
-    if (arr[pacman.x][pacman.y + 1] === 3) { // ha ott ahová megy van kaja, akkor kapunk 10 pontot
-      score = score + 10;
-    }
-    arr[pacman.x][pacman.y + 1] = 1; // ahová pacman megy, ott pacman lesz
-    pacman.y = pacman.y + 1; // pacman koordinátája
-    break;
-}
+      } else {
+        arr[pacman.x][pacman.y] = 0; // ahol pacman eddig állt, az üres lesz
+        arr[pacman.x + 1][pacman.y] = 1; // ahová pacman megy, ott pacman lesz
+        pacman.x = pacman.x + 1; // pacman koordinátája
+      }
+      break;
+    case 'a':
+      if (arr[pacman.x][pacman.y - 1] === 3) { // ha ott ahová megy van kaja, akkor kapunk 10 pontot
+        score = score + 10;
+      }
+      if (arr[pacman.x][pacman.y - 1] === 5) {
 
-console.log(arr);
-console.log(score);
+      } else {
+        arr[pacman.x][pacman.y] = 0; // ahol pacman eddig állt, az üres lesz
+        arr[pacman.x][pacman.y - 1] = 1; // ahová pacman megy, ott pacman lesz
+        pacman.y = pacman.y - 1; // pacman koordinátája
+      }
+      break;
+    case 'd':
+
+      if (arr[pacman.x][pacman.y + 1] === 3) { // ha ott ahová megy van kaja, akkor kapunk 10 pontot
+        score = score + 10;
+      }
+      if (arr[pacman.x][pacman.y + 1] === 5) {
+
+      } else {
+        arr[pacman.x][pacman.y] = 0; // ahol pacman eddig állt, az üres lesz
+        arr[pacman.x][pacman.y + 1] = 1; // ahová pacman megy, ott pacman lesz
+        pacman.y = pacman.y + 1; // pacman koordinátája
+      }
+      break;
+  }
+  return score;
+};
+
+score = move(direct, arr, score, pacman);
+move(direct, arr, score, pacman);
